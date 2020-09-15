35c2c22030de0314fe61fc0edfe2876f2f031ec0
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-09-15 13:08:06 -0400

    Mastery problems (Very Advanced)

---
+++ src/projects/master/master.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.master;
+
+/**
+ *
+ * @author user
+ */
+public class master {
+    
+}
