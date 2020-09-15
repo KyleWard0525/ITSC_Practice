428c50dfd036c8a63f3bf28864d365d4d4742dde
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-09-15 09:26:34 -0400

    print every number between min and max counting by 2

---
+++ src/projects/io/CountByTwo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.io;
+
+/**
+ * For this practice project, print
+ * every number between min and max
+ * counting by 2
+ * 
+ * @author kward60
+ */
+public class CountByTwo {
+    
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 100;
+    }
+    
+}
