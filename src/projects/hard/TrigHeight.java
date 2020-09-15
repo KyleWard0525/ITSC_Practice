39df7023900c50ff9016cae897a7ee68d41da2f4
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-09-15 18:58:31 -0400

    Compute the height of a building given 2 distances and 2 angles of elevation

---
+++ src/projects/hard/TrigHeight.java
@@ -0,0 +1,46 @@
+package projects.hard;
+import java.lang.Math;
+/**
+ * 
+ * A man was standing at a point 250m away
+ * from a building. He observes from that point
+ * that the angle of elevation to the top of the 
+ * building is 24 degrees. He then walks forward
+ * 50m and observes that the angle of elevation to
+ * the top of the building is now 46 degrees.
+ * 
+ * Complete the function to find the height of the building
+ * 
+ * @author kward60
+ */
+public class TrigHeight {
+    
+    private final int distance1 = 250;
+    private final int theta1 = 24;
+    private final int distance2 = 200;
+    private final int theta2 = 46;
+    
+    /**
+     * Compute height of the building
+     * 
+     * Hint:
+     * 1) Notice the java.lang.Math import
+     * 2) If the man is looking UP at the building,
+     * what kind of shape is being formed? And after
+     * he moves?
+     * 
+     * @return 
+     */
+    public static double computeHeight()
+    {
+        double height = 0;
+        
+        
+        return height;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Height = " + computeHeight() + "m");
+    }
+    
+}
