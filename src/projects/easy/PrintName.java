1aff3e8980d520c6f756785be4744ee12fa582f2
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-09-15 16:11:59 -0400

    Print full name using String concat() method

---
+++ src/projects/easy/PrintName.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.easy;
+
+/**
+ * 
+ * For this project, create a string variable
+ * for your first name and another for your last
+ * name. Then, combine the two using the String
+ * concat() method. Finally, print the string:
+ * "Hello, my name is" followed by your name.
+ * 
+ * In order for the solution to be correct the 
+ * output must have a space between each word
+ * 
+ * @author kward60
+ */
+public class PrintName {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
