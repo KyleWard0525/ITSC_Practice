f441291d5b987354b9b0be5e7e2bf5e68a2c29e7
ccomet <43812143+ccomet@users.noreply.github.com>
2020-09-15 14:25:43 -0400

    Add files via upload

---
+++ src/projects/io/solutions/CountByTwo_ccomet.java
@@ -0,0 +1,14 @@
+public class CountByTwo {
+
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 100;
+
+        while (min < max) {
+            min += 2;
+            System.out.println(min);
+        }
+    }
+}
+
+
