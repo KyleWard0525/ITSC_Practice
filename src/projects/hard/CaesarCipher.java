fe93f0fe6c21a829cf236524f21d1d58e368a4cc
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-11-14 23:01:57 -0500

    Caesar Cipher Algorithm

---
+++ src/projects/hard/CaesarCipher.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.hard;
+
+/**
+ * In this problem you are tasked with writing the encryption and
+ * decrpytion methods for a Caesar Cipher algorithm
+ * 
+ * Here's how the Caesar Cipher works:
+ * 
+ * Change a letter to a different one given a shift value
+ * 
+ * Ex) 
+ * 
+ * Text: A
+ * Shift: +2
+ * Cipher: C
+ * 
+ * Ex)
+ * 
+ * Text: Hello
+ * Shift: +5
+ * Cipher: Mjqqt
+ * 
+ * 
+ * @author kward60
+ */
+public class CaesarCipher {
+    
+    public String encrypt(String text, int shift)
+    {
+     String encryptedStr = "";
+     
+     //YOUR CODE HERE
+     
+     
+     return encryptedStr;
+    }
+    
+    
+    public String decrypt(String encryped, int shift)
+    {
+        String decryptedStr = "";
+        
+        //YOUR CODE HERE
+        
+        
+        return decryptedStr;
+    }
+    
+}
