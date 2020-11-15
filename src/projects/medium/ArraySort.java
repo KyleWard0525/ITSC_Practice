a2514482bd7ab20c2c6d76f69b0c285d03c7c246
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-11-14 21:24:07 -0500

    Sort an array of integers

---
+++ src/projects/medium/ArraySort.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.medium;
+
+/**
+ * For this problem you are tasked with writing an algorithm
+ * that will sort an array of integers in ascending order (0,1,2,3,...)
+ * 
+ * 
+ * @author kward60
+ */
+public class ArraySort {
+    
+    public int[] sort(int[] arr)
+    {
+        //YOUR CODE HERE
+        
+        return null;
+    }
+    
+    
+}
