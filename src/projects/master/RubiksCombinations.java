d94648e35c9c826a49661081fee3b6f1a21544ff
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-12-07 05:39:08 -0500

    Rubik's Combinations

---
+++ src/projects/master/RubiksCombinations.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.master;
+
+/**
+ * Welcome to the Thunderdome that is the Master level! 
+ * The level of Master is not just a name. These problems
+ * are designed to not only be extremely challenging but 
+ * may also incorporate advanced concepts and techniques.
+ * That being said,
+ * 
+ * In this problem you are tasked with computing the number of 
+ * all possible patterns/combinations that can be made by an 
+ * n-by-n Rubik's cube. 
+ * 
+ * There are a few general rules that hold for all size cubes:
+ * 
+ * 1) Every cube will have 8 corner pieces, each of which has 8 possible permutations
+ * 2) The corner pieces can be oriented in 3 different ways 
+ * 3) 3x3 has 12, 4x4 has 24, 5x5 has 36, and so on..
+ * 
+ * 
+ * @author kward60
+ */
+public class RubiksCombinations {
+    
+}
