f836c5d10d2fe892d80d0b32db44e03111825ae1
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-12-07 01:00:05 -0500

    Popular tech interview question

---
+++ src/projects/hard/Palindrome.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.hard;
+
+/**
+ *
+ * In this problem you will be given a string as 
+ * a parameter and you must determine whether or not
+ * that string is a palindrome. 
+ * 
+ * A Palindrome is a word that is spelled the same forwards
+ * as it is backwards (i.e Racecar, Anna, Madam)
+ * 
+ * You can assume that each string will be only 1 word in length
+ * (no need for splitting)
+ * 
+ * 
+ * THIS IS A VERY POPULAR TECHNICAL INTERVIEW QUESTION
+ * 
+ * 
+ * @author kward60
+ */
+public class Palindrome {
+    
+    /**
+     * Check if string is a palindrome
+     * @param msg
+     * @return 
+     */
+    public boolean isPalindrome(String msg)
+    {
+        boolean palindrome = false;
+        
+        //Your code here
+        
+        return palindrome;
+    }
+    
+    
+}
