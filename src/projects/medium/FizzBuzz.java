20d51b7cd5cdee851972051fb3515c8adf6fb835
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2020-12-07 01:53:11 -0500

    FizzBuzz problem

---
+++ src/projects/medium/FizzBuzz.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.medium;
+
+/**
+ * This is the popular FizzBuzz problem
+ * 
+ * In this problem you must print every
+ * number from 1-n. n being the integer passed
+ * in as a parameter. 
+ * 
+ * BUT, if the current number is a multiple of 3
+ * print "fizz" instead. Furthermore, if the number
+ * is a multiple of 5, print "buzz". If the number is
+ * both a multiple of 3 AND 5 then print "fizzbuzz"
+ * 
+ * Ex) n = 10
+ * 1
+ * 2
+ * fizz
+ * 4
+ * buzz
+ * fizz
+ * 7
+ * 8
+ * 9
+ * buzz
+ * 
+ * THIS IS ALSO A VERY POPULAR CODING CHALLENGE FOR 
+ * TECHNICAL INTERVIEWS
+ * 
+ * 
+ * @author kward60
+ */
+public class FizzBuzz {
+    
+    public void fizzBuzz(int n)
+    {
+        //Your code here
+        
+    }
+    
+}
