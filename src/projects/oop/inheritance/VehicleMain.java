57fdb926b13200454a0f51d6790a372156fdca06
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-03-11 15:06:37 -0500

    Test Harness

---
+++ src/projects/oop/inheritance/VehicleMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects.oop.inheritance;
+
+/**
+ * This is where you will write the test harness for your solution.
+ * 
+ * Create a car object, set its values for gallonsPerTank and mpg then,
+ * call the car object's toString() method and ensure all the information
+ * is correct.
+ * 
+ * @author kward60
+ */
+public class VehicleMain {
+    
+    public static void main(String[] args) {
+        //Create a car object
+        
+        //Set gallonsPerTank
+        
+        //Set mpg
+        
+        //Call toString() and validate output
+        
+    }
+    
+    
+}
